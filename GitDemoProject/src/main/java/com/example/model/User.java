aeadfee5ab6c7eee3e9fe82d64a9117d6ff08373
StudentOne <guidocv72+StudentOne@gmail.com>
2024-06-09 16:12:20 +0200

    Creazione parziale classe model.User

---
+++ GitDemoProject/src/main/java/com/example/model/User.java
@@ -0,0 +1,22 @@
+package com.example.model;
+
+public class User {
+
+	private Long id;
+	private String nome;
+	private String cognome;
+	private String username;
+	private String password;
+	
+	public User(Long id, String nome, String cognome, String username, String password) {
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+	
+	
+}
